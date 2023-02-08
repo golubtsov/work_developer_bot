6d28f16e8305f25fa94fb74d35203b0eab9c00c3
unknown <nproger781@gmail.com>
2023-02-08 09:13:53 +0700

    create_info_job

--- bot.js
+++ bot.js
@@ -11,10 +11,10 @@ const {
 const Job = require('./module/Job');
 
 
-let count = 0;
-let id_job;
-let info_job;
-let name_lang;
+let count = 0; // счетчик, чтобы получать следующую вакансию из массива
+let id_job; // id вакансии из массива
+let info_job; // весь объект вакансии из массива
+let name_lang; // имя выбранного языка программирования
 
 const bot = new Telegraf(process.env.API_BOT);
 
@@ -25,15 +25,15 @@ bot.help((ctx) => ctx.reply(BOT_COMANDS));
 
 bot.command('jobs', async (ctx) => {
     try {
-        str_find_name = 'Язык программирования не указан. Чтобы начать просмотр вакансий, наберите /jobs и выбирите язык.';
-        count = 0;
         await ctx.reply('Выбирите язык программирования', LANDS_KEYBOARD);
     } catch (e) {
         console.error(e);
     }
 });
 
+// ф-ия получает массив вакансий, получает элемент согласно count, отправляет запрос на полученик более подробной иформации о вакансии
 async function get_jobs(name) {
+    // когда вызываем ф-ию get_next_job, у нас нет данных о выбранном языке, т.к. кнопка "Смотреть еще" одна, а кнопки с языками передают имена языков, для решения этой проблемы использую строку 37, во время получения первой вакансии в name_lang кладем выбранный язык, после этого просматриваю след. вакансии будет тот же язык программирования
     name_lang = name;
     await fetch(`https://api.hh.ru/vacancies/?text=${name}`)
         .then(data => data.json())
@@ -47,27 +47,32 @@ async function get_jobs(name) {
         })
 }
 
+// ф-ия создает на основе класса Job сообщение о вакансии
+function create_info_job(job){
+    let el_job = new Job(
+        job.id,
+        job.name,
+        job.salary,
+        job.address,
+        job.experience.name,
+        job.key_skills
+    );
+
+    el_job.check_salary();
+    el_job.check_from_to();
+    el_job.check_adress();
+    el_job.check_skills();
+
+    let note = el_job.create_note();
+    return note;
+}
+
 function get_action_btn(name) {
     try {
         bot.action(name, async (ctx) => {
             await ctx.answerCbQuery();
             await get_jobs(name);
-
-            let job = new Job(
-                info_job.id,
-                info_job.name,
-                info_job.salary,
-                info_job.address,
-                info_job.experience.name,
-                info_job.key_skills
-            );
-
-            job.check_salary();
-            job.check_from_to();
-            job.check_adress();
-            job.check_skills();
-            let note = job.create_note();
-
+            let note = create_info_job(info_job);
             await ctx.reply(note, MORE_INFO_KEYBOARD);
 
             count++;
@@ -82,22 +87,7 @@ function get_next_job() {
         bot.action('watch', async (ctx) => {
             await ctx.answerCbQuery();
             await get_jobs(name_lang);
-
-            let job = new Job(
-                info_job.id,
-                info_job.name,
-                info_job.salary,
-                info_job.address,
-                info_job.experience.name,
-                info_job.key_skills
-            );
-
-            job.check_salary();
-            job.check_from_to();
-            job.check_adress();
-            job.check_skills();
-            let note = job.create_note();
-
+            let note = create_info_job(info_job);
             await ctx.reply(note, MORE_INFO_KEYBOARD);
 
             count++;
